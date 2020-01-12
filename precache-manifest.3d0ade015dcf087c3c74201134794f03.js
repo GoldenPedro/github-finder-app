96fdc6fa82d35e99b05e325c5f162687d17a459b
Pedro <pedropablo@casuso.us>
2020-01-11 22:47:35 -0600

    Updates

---
+++ precache-manifest.3d0ade015dcf087c3c74201134794f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c044dec1ef8f020e34f7618d449088",
+    "url": "/github-finder-app/index.html"
+  },
+  {
+    "revision": "cc967c87fce98f107cf7",
+    "url": "/github-finder-app/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "5f8036004d5f48070cbc",
+    "url": "/github-finder-app/static/js/2.08a1248e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-finder-app/static/js/2.08a1248e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc967c87fce98f107cf7",
+    "url": "/github-finder-app/static/js/main.2eb87fc0.chunk.js"
+  },
+  {
+    "revision": "67e1ae55c765adeeffa3",
+    "url": "/github-finder-app/static/js/runtime-main.587ff894.js"
+  }
+]);
