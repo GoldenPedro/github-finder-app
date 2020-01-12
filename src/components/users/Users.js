ad707d9875c56c287728379f27cb9a39e6d008a7
Pedro <pedropablo@casuso.us>
2020-01-11 19:18:01 -0600

    added search functionality

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
@@ -20,7 +20,7 @@ const Users = ({ users, loading}) => {
 
 Users.propTypes = {
     users: PropTypes.array.isRequired,
-    users: PropTypes.bool.isRequired,
+    loading: PropTypes.bool.isRequired,
 }
 
 const userStyle= {
